9cd3b1809331c409848d0dd6ddb3ca49156327e7
Buontempo-Raul <162702278+Buontempo-Raul@users.noreply.github.com>
2024-06-27 10:03:04 +0300

    Add files via upload

---
+++ keys - Copy.py
@@ -0,0 +1,8 @@
+# information used for the sms function
+# you need to create an account at twilio and get these credentials from them
+
+account_sid=''
+auth_token=''
+
+twilio_number=''
+my_phone_number=''
